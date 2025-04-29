863572eb48b296c4cb267435af7799bb54dab624
andregarcia0412 <andregarcia0412@gmail.com>
2025-04-29 20:11:29 -0300

    isShuffledWell.js

--- isShuffledWell.js
+++ isShuffledWell.js
@@ -1,6 +1,3 @@
-var input = require('fs').readFileSync('stdin', 'utf8');
-var lines = input.split(/\s+/);
-
 function isShuffledWell(arr){
     for(var i = 0; i < arr.length; i++){
         if(Math.abs(arr[i] - arr[i + 1]) == 1 && Math.abs(arr[i + 1] - arr[i + 2]) == 1){
